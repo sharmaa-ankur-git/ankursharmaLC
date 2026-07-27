7a282ec802316ab96f3e62e2da0d8fd652cc9d30
sharmaa-ankur-git <sharmaankur69851@gmail.com>
2026-07-27 22:22:19 +0530

    Time: 7 ms (38.22%), Space: 19.9 MB (20.76%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -9,9 +9,7 @@ class Solution:
             current_max = max_product
             current_min = min_product
             max_product = max(num, current_max * num, current_min * num)
-            min_product = min(num, current_max * num, current_min * num)
-            
+            min_product = min(num, current_max * num, current_min * num)   
             result = max(result, max_product)
-        
         return result
         
