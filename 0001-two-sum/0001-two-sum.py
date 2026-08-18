5b1d52312feb8f94bd1b5849b2d9eb482a74c09c
Ankur Kumar Sharma <sharmaankur69851@gmail.com>
2026-08-18 20:58:57 +0530

    Time: 2410 ms (7.72%), Space: 13.3 MB (16.74%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,10 +1,6 @@
 class Solution(object):
-    def twoSum(self, nums, target):
-        sum=0
+    def twoSum(self, nums, target):   
         n=len(nums)
-        left=0
-        right=n-1
-        
         for i in range(n):
             for j in range(i+1,n):
                 sum=nums[i]+nums[j]
