56d841707cbefac80f6a335c86750684a724d78f
Ankur Kumar Sharma <sharmaankur69851@gmail.com>
2026-09-19 21:46:49 +0530

    Time: 435 ms (5.65%), Space: 19.4 MB (40.54%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -1,10 +1,10 @@
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
         chars=list(text)
-        count=0
+        count=0                               #still unatural way to do it for me
         while True:
             for char in "balloon":
-                if char in chars:
+                if char in chars:              
                     chars.remove(char)
                 else:
                     return count
